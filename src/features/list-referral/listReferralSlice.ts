766850b036739f0f1408cea12ce2985a07ec5463
ReyGab <reyiel0528@gmail.com>
2026-02-05 11:49:17 +0800

    MOD: Update api to transfer to reducers

--- src/features/list-referral/listReferralSlice.ts
+++ src/features/list-referral/listReferralSlice.ts
@@ -1,4 +1,3 @@
-import {  createAsyncThunk } from "@reduxjs/toolkit";
 import { createAppSlice } from "../../app/createAppSlice"
 import { IReferral, IReferralSliceState } from "../../interfaces/referalls.interface";
 
@@ -8,57 +7,72 @@ const initialState: IReferralSliceState = {
   status: "idle",
 }
 
-export const getReferralsApi = createAsyncThunk<IReferral[]>('referral/get', async () => {
-  const response = await fetch(`http://localhost:9092/referrals`);
-  console.log(response, 'response');
-   if (!response.ok) {
-      throw new Error('Failed to fetch referrals');
-    }
-
-    return (await response.json()) as IReferral[];
-})
-
-export const deleteReferralApi = createAsyncThunk<IReferral, string>('referral/delete', async (id) => {
-  const response = await fetch(`http://localhost:9092/referrals/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    }
-  });
-  console.log(response, 'response');
-   if (!response.ok) {
-      throw new Error('Failed to delete referrals');
-    }
-
-    return { id } as IReferral;
-})
-
 export const referralSlice = createAppSlice({
   name: 'referral',
   initialState,
-    reducers: {
-    getReferralsInfo: (state, action) => {
-      state.referralInfo = action.payload
-    }
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(getReferralsApi.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(getReferralsApi.fulfilled, (state, action) => {
-        state.status = 'idle';
-        state.referralInfo = action.payload;
-      })
-      .addCase(getReferralsApi.rejected, (state) => {
-        state.status = 'failed';
-      })
-      .addCase(deleteReferralApi.fulfilled, (state, action) => {
-      state.referralInfo = state.referralInfo.filter(
-        referral => referral.id !== action.payload.id
-      );
-      });
-  },
+  reducers: (create) => ({
+    getReferralsApi: create.asyncThunk(
+      async (_, { rejectWithValue }) => {
+        const response = await fetch(`http://localhost:9092/referrals`);
+        if (!response.ok) {
+          return rejectWithValue('Failed to fetch referrals');
+        }
+        return (await response.json()) as IReferral[];
+      },
+      {
+        pending: (state) => {
+          state.status = 'loading';
+        },
+        fulfilled: (state, action) => {
+          state.status = 'idle';
+          state.referralInfo = action.payload;
+        },
+        rejected: (state) => {
+          state.status = 'failed';
+        },
+      }
+    ),
+    deleteReferralApi: create.asyncThunk(
+      async (id: string) => {
+          const response = await fetch(`http://localhost:9092/referrals/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          }
+        });
+        if (!response.ok) {
+            throw new Error('Failed to delete referrals');
+          }
+
+        return { id } as IReferral;
+      },
+      {
+        fulfilled: (state, action) => {
+          state.referralInfo = state.referralInfo.filter(
+            r => r.id !== action.payload.id
+          );
+        },
+      }
+    )
+  }),
+  // extraReducers: (builder) => {
+  //   builder
+  //     .addCase(getReferralsApi.pending, (state) => {
+  //       state.status = 'loading';
+  //     })
+  //     .addCase(getReferralsApi.fulfilled, (state, action) => {
+  //       state.status = 'idle';
+  //       state.referralInfo = action.payload;
+  //     })
+  //     .addCase(getReferralsApi.rejected, (state) => {
+  //       state.status = 'failed';
+  //     })
+  //     .addCase(deleteReferralApi.fulfilled, (state, action) => {
+  //     state.referralInfo = state.referralInfo.filter(
+  //       referral => referral.id !== action.payload.id
+  //     );
+  //     });
+  // },
   selectors: {
     selectedReferralInfo: state => state.referralInfo,
     selectedStatus: state => state.status,
@@ -66,3 +80,5 @@ export const referralSlice = createAppSlice({
 });
 
 export const { selectedReferralInfo, selectedStatus } = referralSlice.selectors
+
+export const { getReferralsApi, deleteReferralApi } = referralSlice.actions;
